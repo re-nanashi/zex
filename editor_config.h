f3af7ddef4c8ff4ce371a2ea8e5bb631806a4de0
re-nanashi <jfabro3124@gmail.com>
2024-02-02 04:18:56 +0800

    style: Change comment style

--- editor_config.h
+++ editor_config.h
@@ -12,45 +12,45 @@
 #include <termios.h>
 #include <time.h>
 
-/** @brief A row in the editor */
+/* @brief A row in the editor */
 typedef struct editor_row {
-    /** size of the char buffer */
+    /* size of the char buffer */
     int size;
-    /** size of the render buffer */
+    /* size of the render buffer */
     int rsize;
-    /** text buffer */
+    /* text buffer */
     char *chars;
-    /** text buffer to render to the screen */
+    /* text buffer to render to the screen */
     char *render;
 } erow_t;
 
-/** @brief Editor global config */
+/* @brief Editor global config */
 typedef struct editor_config {
-    /** cursor coordinates */
+    /* cursor coordinates */
     int cx, cy;
-    /** cursor horizontal coordinates for render */
+    /* cursor horizontal coordinates for render */
     int rx;
-    /** vertical scroll row offset */
+    /* vertical scroll row offset */
     int row_offset;
-    /** horizontal scroll col offset */
+    /* horizontal scroll col offset */
     int col_offset;
-    /** terminal number of rows */
+    /* terminal number of rows */
     int screenrows;
-    /** terminal number of cols */
+    /* terminal number of cols */
     int screencols;
-    /** opened file num of rows */
+    /* opened file num of rows */
     int numrows;
-    /** array of all rows with text */
+    /* array of all rows with text */
     erow_t *rows;
-    /** changes counter */
+    /* changes counter */
     int dirty;
-    /** filename str */
+    /* filename str */
     char *filename;
-    /** status message str */
+    /* status message str */
     char statusmsg[80];
-    /** status message timeout time */
+    /* status message timeout time */
     time_t statusmsg_time;
-    /** original termios config */
+    /* original termios config */
     struct termios orig_termios;
 } econf_t;
 
