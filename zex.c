cafb5e1dd8ef2e19e3e52833737c18c069a0fd69
re-nanashi <jfabro3124@gmail.com>
2024-02-02 06:06:12 +0800

    feat: Read cursor position using terminal dimensions

--- zex.c
+++ zex.c
@@ -60,13 +60,46 @@ enable_raw_mode()
 }
 
 /**
+ * @brief Get the current position of cursor on the terminal
+ *
+ * @param rows Pointer to row property of config
+ * @param cols Pointer to cols property of config
+ */
+int
+get_cursor_pos(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int idx = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1; // no cursor position reported
+
+    // Read escape code to buffer
+    while (idx < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[idx], 1) != 1) break;
+        if (buf[idx] == 'R') break;
+        idx++;
+    }
+    buf[idx] = '\0';
+
+    // Parse the buffer
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d:%d", rows, cols) != 2) return -1;
+    *rows -= 2; // subtract 2 to give space to  status bar and cmd line
+
+    editor_read_key();
+
+    return -1;
+}
+
+/**
  * @brief Get the current window size of the terminal
  *
  * @param rows Pointer to row property of config
  * @param cols Pointer to cols property of config
  */
 int
-get_window_size(int *rows, int *cols)
+get_window_sz(int *rows, int *cols)
 {
     struct winsize ws;
 
@@ -74,7 +107,7 @@ get_window_size(int *rows, int *cols)
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         // Move the cursors to the edge of the terminal
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-        return get_cursor_position(rows, cols);
+        return get_cursor_pos(rows, cols);
     }
     else {
         // Subtract 2 from rows to give space to status bar and command line
@@ -100,7 +133,7 @@ init_editor()
     econfig.statusmsg[0] = '\0';
     econfig.statusmsg_time = 0;
 
-    if (get_window_size(&econfig.screenrows, &econfig.screencols) == -1)
+    if (get_window_sz(&econfig.screenrows, &econfig.screencols) == -1)
         die("get_window_size");
 }
 
