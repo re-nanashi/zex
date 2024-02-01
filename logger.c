0e0358572c5b61ae2b81d2f772ad0c460880a28b
re-nanashi <jfabro3124@gmail.com>
2024-02-02 07:36:52 +0800

    feat: Init logger

---
+++ logger.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "logger.h"
+
+void
+die(const char *s)
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clears the screen; refer to VT100
+    write(STDOUT_FILENO, "\x1b[H", 3); // reposition cursor to top
+
+    perror(s);
+    exit(1);
+}
---
+++ logger.h
@@ -0,0 +1,17 @@
+/**
+ * @file logger.h
+ * @author re-nanashi
+ * @brief Logger functions and macros for logging messages and events in Zex.
+ */
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+/**
+ * @brief Log err message then exit program
+ *
+ * @param s Error message string
+ */
+void die(const char *s);
+
+#endif /* LOGGER_H */
