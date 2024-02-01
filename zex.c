724b8af349f63ec8ce33dc427a6e3f4572303a90
re-nanashi <jfabro3124@gmail.com>
2024-02-02 04:13:18 +0800

    refactor: Error logger before exit

--- zex.c
+++ zex.c
@@ -4,6 +4,21 @@
 /* @brief Editor configurations */
 econf_t econfig;
 
+/**
+ * @brief Log err message then exit program
+ *
+ * @param s Error message string
+ */
+void
+die(const char *s)
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clears the screen; refer to VT100
+    write(STDOUT_FILENO, "\x1b[H", 3); // reposition cursor to top
+
+    perror(s);
+    exit(1);
+}
+
 /* @brief Disable raw mode/non-canonical mode */
 void
 disable_raw_mode()
